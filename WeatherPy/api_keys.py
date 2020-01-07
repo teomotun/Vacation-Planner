edbaae36bc78c3104694824b4241ac47208c42ef
tolu <teomotun@ncsu.edu>
2020-01-07 16:35:33 -0500

    Updated WeatherPy.ipynb

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0008e55f806f895e555cebbb7a249d71"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDeJW7z55_Mj8dcPpo8rIPFiNX7SqOWLS0"
